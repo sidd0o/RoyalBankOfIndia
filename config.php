f103d49d1f33989ad2651f750c5c1eb5f5742567
sidd0o <72683321+sidd0o@users.noreply.github.com>
2021-04-12 10:38:58 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "Indian_bank";
+	$dbname = "indian_bank";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
